218b136f082afe7158f2a6ab1100c287b6e8806c
edvin <edvin@DESKTOP-QHD6AI8>
2022-10-04 20:06:05 +0200

    Add project files.

---
+++ Uppgift 3.21/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett matematiskt uttryck med endast addition.");
+            Console.Write("\nSkriv här: "); string[] input = Console.ReadLine().Split('+');
+
+            int[] tal = Array.ConvertAll(input, int.Parse);
+
+            int summa = tal.Sum();
+
+            Console.WriteLine($"\nSumman blir då {summa}.");
+        }
+    }
+}
